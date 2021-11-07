a0e5439e47f7abc7cf5e88ce7eec84927abf21d3
Ruben Gueorguiev <scoutspyz@Hotmail.com>
2021-11-07 18:38:26 +1300

    Added basic inventory
    Removed inventory from home screen and replaced it with a basic dedicated inventory page. More changes to attempt to fix audio, seems to work well on web but not iOS.

--- components/AudioManager.js
+++ components/AudioManager.js
@@ -9,10 +9,11 @@ class AudioManager {
   playAsync = async (name, isLooping) => {
     if (name in this.sounds) {
       const soundObject = this.sounds[name]
+      console.log(this.sounds)
       try {
-        await soundObject.setPositionAsync(0)
-        await soundObject.setIsLoopingAsync(isLooping)
-        await soundObject.playAsync()
+        //await soundObject.setIsLoopingAsync(isLooping)
+        //await soundObject.setPositionAsync(0)
+        await soundObject.playFromPositionAsync(0)
       } catch ({ message }) {
         console.warn(`Error playing audio: ${message}`)
       }
@@ -52,7 +53,11 @@ class AudioManager {
     for (let key of keys) {
       const item = Assets.audio[key]
       this.sounds[key.split('.')[0]] = (
-        await Audio.Sound.createAsync(item)
+        await Audio.Sound.createAsync(item, {
+          shouldPlay: false,
+          progressUpdateIntervalMillis: 1,
+          downloadFirst: true,
+        })
       ).sound
     }
   }
