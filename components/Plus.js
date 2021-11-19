c6ec226e3120f784497a1cd0ff5db14c6975d64a
Ruben Gueorguiev <scoutspyz@Hotmail.com>
2021-11-19 13:34:46 +1300

    Fixed Plus Visibility

--- components/Plus.js
+++ components/Plus.js
@@ -3,17 +3,17 @@ import { Pressable, StyleSheet, View, Text, Animated } from 'react-native'
 
 const Plus = ({ currentBlockColour, currentTool, currentBlock }, props) => {
   const fadeAnim = useRef(new Animated.Value(0)).current
-  const riseAnim = useRef(new Animated.Value(550)).current
+  const riseAnim = useRef(new Animated.Value(650)).current
   const [horizontalPosition, setHorizontalPosition] = useState(-50)
 
   useEffect(() => {
     setHorizontalPosition(
       (horizontalPosition) =>
-        (horizontalPosition = Math.random() * (-45 - -55) + -55)
+        (horizontalPosition = Math.random() * (-55 - -60) + -55)
     )
     Animated.sequence([
       Animated.timing(riseAnim, {
-        toValue: 550,
+        toValue: 650,
         duration: 1,
         useNativeDriver: false,
       }),
